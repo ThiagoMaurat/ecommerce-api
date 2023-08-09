64c932377630d98a75d979c31f32415764f64aab
ThiagoMaurat <thiagomaurat@hotmail.com>
2023-08-08 21:58:01 -0300

    feat:category entities

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -2,8 +2,8 @@ import { describe, test } from "node:test";
 import { Category } from "./category";
 import assert from "node:assert";
 
-describe("Category Test", () => {
-  test("Category Test", () => {
+describe("Category Unit Tests", () => {
+  test("constructor of Category Test", () => {
     const category = new Category("Movie");
     assert.strictEqual("Movie", category.name);
   });
